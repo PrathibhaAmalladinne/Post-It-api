d320b80385b831f4ced1c0521e52e094827b6750
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-03 03:12:03 +0530

    ready to deploy backend agains

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://ziraa.netlify.app/"]
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ziraa.netlify.app/",
+  "https://post-it-cbah.onrender.com",
+]
 
 module.exports = allowedOrigins
