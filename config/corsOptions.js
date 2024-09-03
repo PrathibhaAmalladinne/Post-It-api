848a78bbade6c52bcfbea3885c348c4d0d722df3
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-09-03 16:21:30 +0530

    kinda sure

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,20 @@
-const allowedOrigins = require("./allowedOrigins")
+// const allowedOrigins = require("./allowedOrigins")
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// }
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (origin === "https://ziraa.netlify.app/") {
       callback(null, true)
     } else {
       callback(new Error("Not allowed by CORS"))
